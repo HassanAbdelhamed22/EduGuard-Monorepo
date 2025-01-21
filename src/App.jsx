3045a89bac140b76d91846d30319d56a03d5d33c
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-21 18:00:28 +0200

    create validationSchema with yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Toaster } from "react-hot-toast";
+import Login from "./pages/Login";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <Toaster position="top-right" />
+      <Login />
     </>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(6, "Password must be at least 6 characters")
+    .max(20, "Password must not exceed 20 characters")
+    .matches(
+      /^[A-Z][a-zA-Z0-9]{5,}$/,
+      "Password must start with an uppercase letter and contain at least 6 characters"
+    ),
+});
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
 
-export default Login
+  const initialValues = {
+    email: "",
+    password: "",
+  }
+
+  
+  return <div>Login</div>;
+};
+
+export default Login;
