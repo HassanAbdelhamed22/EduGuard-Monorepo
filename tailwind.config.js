8a342da42e0889c04659510060c8326aab6db7ff
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-21 17:04:41 +0200

    add colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4D46B4",
+        primaryHover: "#3e3890",
+        background: "#ffffff",
+        darkGray: "#3A3E45",
+        lightGray: "#E9ECEF",
+        borderLight: "#e5e7eb",
+      },
+    },
   },
   plugins: [],
 };
