e63a9f1e3048eaf54bf90abd442cbd2563a6c451
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-21 22:06:40 +0200

    update AdminRoutes.jsx

---
+++ src/routes/AdminRoutes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import SecureRoute from "./SecureRoute";
+import Dashboard from './../pages/admin/Dashboard';
+import AdminLayout from "../layouts/AdminLayout";
+
+const AdminRoutes = () => {
+  return (
+    <Routes>
+      <Route element={<SecureRoute allowedRoles={["admin"]} />}>
+        <Route element={<AdminLayout />}>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          {/* Add more admin-specific routes */}
+        </Route>
+      </Route>
+    </Routes>
+  );
+};
+
+export default AdminRoutes;
