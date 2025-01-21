8c7e544f32091cd1cda6dfa8dc4bc412e411857c
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-21 21:37:27 +0200

    update Professor Layout

--- src/layouts/ProfessorLayout.jsx
+++ src/layouts/ProfessorLayout.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const ProfessorLayout = () => {
   return (
-    <div>ProfessorLayout</div>
-  )
-}
+    <div className="professor-layout">
+      <header>Professor Header</header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>Professor Footer</footer>
+    </div>
+  );
+};
 
-export default ProfessorLayout
+export default ProfessorLayout;
