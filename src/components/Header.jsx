3e50a6992e0959e33f980c94f11accd8096641dd
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-23 08:43:14 +0200

    put default img if no profile picture

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import { Search, Bell, List } from "lucide-react";
+import { Bell } from "lucide-react";
 import { profilePicture, username, userRole } from "../constants";
 import Logo from "./Logo";
 import SearchBar from "./ui/SearchBar";
 
 const Header = () => {
+  const getInitials = (name) => name.charAt(0).toUpperCase();
+
+  console.log("Profile Picture:", profilePicture); // Debugging log
+  console.log("Username:", getInitials(username)); // Debugging log
+
+  // Check if profilePicture exists and is not an empty string
+  const hasValidProfilePicture = profilePicture && profilePicture.trim() !== "" && profilePicture !== "null";
+
   return (
     <header className="flex items-center justify-between px-5 py-2 bg-white shadow-sm">
       {/* LOGO*/}
@@ -18,17 +26,26 @@ const Header = () => {
 
       {/* User Info*/}
       <div className="flex items-center gap-4">
-        <Bell className="w-6 h-6 cursor-pointer text-black-900" />
+        <Bell className="w-6 h-6 cursor-pointer text-darkGray" />
         <div className="flex items-center gap-2">
           <div className="text-right">
             <div className="font-semibold">{username}</div>
-            <div className="text-sm text-purple-600">{userRole}</div>
+            <div className="text-sm text-primary">{userRole}</div>
           </div>
-          <img
-            src={profilePicture}
-            alt="User avatar"
-            className="w-12 h-12 ml-3 rounded-full"
-          />
+          {hasValidProfilePicture ? (
+            <img
+              src={profilePicture}
+              alt={`${username}'s avatar`}
+              className="w-12 h-12 ml-3 rounded-full"
+            />
+          ) : (
+            <div
+              className="w-10 h-10 ml-3 flex items-center justify-center rounded-full bg-primary text-white font-semibold"
+              title={username}
+            >
+              {getInitials(username)}
+            </div>
+          )}
         </div>
       </div>
     </header>
