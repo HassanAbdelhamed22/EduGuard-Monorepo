46bd185883ceed735d4b60ca7ae0474acfc44a8f
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-23 08:55:29 +0200

    remove profile picture from localstorage when login

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,14 +2,13 @@ export const saveUserData = (data) => {
   localStorage.setItem("token", data.token);
   localStorage.setItem("username", data.name);
   localStorage.setItem("role", data.role);
-  localStorage.setItem("profile_picture", data.profile_picture);
 };
 
 export const removeUserData = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("username");
   localStorage.removeItem("role");
-  localStorage.removeItem("profile_picture");
+  localStorage.removeItem("profilePicture");
 };
 
 
