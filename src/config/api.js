8479f858933d9ac02119b5487b9a55b37204f699
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-23 22:01:13 +0200

    fix some issues in api config

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { token, tokenExpiry } from "../constants";
 import toast from "react-hot-toast";
+import { removeUserData } from "../utils/functions";
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api/",
@@ -12,27 +13,33 @@ const api = axios.create({
 // Add a request interceptor
 api.interceptors.request.use(
   async (config) => {
-    if (token && Date.now() > tokenExpiry) {
+    if (token && Date.now() > Number(tokenExpiry)) {
       try {
-        const refreshResponse = await axios.post("auth/refresh", null, {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
+        const refreshResponse = await api.post(
+          "http://127.0.0.1:8000/api/auth/refresh",
+          null,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        console.log("Refresh Response:", refreshResponse.data);
 
         const newToken = refreshResponse.data.token;
         localStorage.setItem("token", newToken);
         localStorage.setItem(
           "tokenExpiry",
-          Date.now() + refreshResponse.data.tokenExpiry * 1000
+          (Date.now() + Number(refreshResponse.data.expires_in) * 1000).toString()
         );
 
         config.headers.Authorization = `Bearer ${newToken}`;
       } catch (error) {
-        toast.error("Error refreshing token:", error);
-        localStorage.removeItem("token");
-        localStorage.removeItem("tokenExpiry");
+        toast.error("Session expired. Please log in again");
+        removeUserData();
         window.location.href = "/login";
+        return Promise.reject(error);
       }
     } else if (token) {
       config.headers.Authorization = `Bearer ${token}`;
