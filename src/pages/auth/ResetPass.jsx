44ca9f25a18f7c73c4c01036d98bb78bb21c1434
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-24 18:26:13 +0200

    create handleSubmit in ResetPass component

--- src/pages/auth/ResetPass.jsx
+++ src/pages/auth/ResetPass.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from "react";
+import { resetPass } from "./../../services/authService";
 
 const ResetPass = () => {
-  return (
-    <div>ResetPass</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(false);
 
-export default ResetPass
+  async function handleSubmit(value) {
+    setIsLoading(true);
+    try {
+      const { data, status } = await resetPass(value);
+
+      if (status === 200) {
+        toast.success(data.message);
+
+        setTimeout(() => {
+          window.location.href = "/send-code";
+        }, 2000);
+      } else {
+        toast.error("Unexpected server response. Please try again.");
+      }
+    } catch (error) {
+      const errorMessage =
+        error.response?.data?.message ||
+        "Something went wrong. Please try again.";
+      toast.error(errorMessage);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  return <div>ResetPass</div>;
+};
+
+export default ResetPass;
