847e97c31c3ca998d515c9af3ce8bcdf1a6aafc6
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-25 09:26:12 +0200

    create handleSubmit function

--- src/pages/auth/SendCode.jsx
+++ src/pages/auth/SendCode.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import { sendCode } from "./../../services/authService";
+import { resetPassValidationSchema } from "../../utils/validation";
+import { Form, Formik, useFormik } from "formik";
+import Input from "../../components/ui/Input";
+import Button from "../../components/ui/Button";
+import Logo from "../../components/Logo";
+import img from "../../assets/auth/forgot-password.svg";
+import { useNavigate } from "react-router";
+import toast from "react-hot-toast";
 
 const SendCode = () => {
-  return (
-    <div>SendCode</div>
-  )
-}
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
-export default SendCode
+  async function handleSubmit(value) {
+    setIsLoading(true);
+    try {
+      const { data, status } = await sendCode(value);
+
+      if (status === 200) {
+        toast.success(
+          "Password reset link sent successfully, you will navigate to the send code page after 2 seconds!"
+        );
+
+        setTimeout(() => {
+          window.location.href = "/send-code";
+        }, 2000);
+      } else {
+        toast.error("Unexpected server response. Please try again.");
+      }
+    } catch (error) {
+      const errorMessage =
+        error.response?.data?.message ||
+        "Something went wrong. Please try again.";
+      toast.error(errorMessage);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const initialValues = {
+    email: "",
+  };
+  return <div>SendCode</div>;
+};
+
+export default SendCode;
