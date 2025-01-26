c890705ccbf72ec7fde8e5e08cca60740e6a0b09
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-26 16:43:51 +0200

    create defaultProfile object and states

--- src/pages/auth/Profile.jsx
+++ src/pages/auth/Profile.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const defaultProfile = {
+  name: "",
+  email: "",
+  phone: "",
+  address: "",
+  profile_picture: "",
+}
 
 const Profile = () => {
+  const [profile, setProfile] = useState(defaultProfile);
+  const [isUploading, setIsUploading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  
   return (
     <div>Profile</div>
   )
