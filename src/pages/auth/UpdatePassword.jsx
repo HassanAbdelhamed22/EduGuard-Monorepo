8a0e1db5e1fa73ee26fe9b9277b74a7565088e3d
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-26 23:51:29 +0200

    create handleSubmit function

--- src/pages/auth/UpdatePassword.jsx
+++ src/pages/auth/UpdatePassword.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { updatePassword } from "../../services/authService";
+import toast from "react-hot-toast";
 
 const UpdatePassword = () => {
-  return (
-    <div>UpdatePassword</div>
-  )
-}
+  const initialValues = {
+    current_password: "",
+    new_password: "",
+    new_password_confirmation: "",
+  };
 
-export default UpdatePassword
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+    setSubmitting(true);
+
+    try {
+      const { data, status } = await updatePassword(values);
+
+      if (status === 200) {
+        toast.success(data.message);
+        resetForm();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error("Error updating password:", error);
+      toast.error(error.response?.data?.message || "An error occurred.");
+    } finally {
+      setSubmitting(false);
+    }
+  };
+  return <div>UpdatePassword</div>;
+};
+
+export default UpdatePassword;
