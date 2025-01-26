f56176b758c3d2124e47d8a2201104cb1b571107
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-26 22:06:30 +0200

    update uploadProfilePicture and deleteProfilePicture to do not save picture profile url in local storage

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,47 +1,20 @@
 import toast from "react-hot-toast";
 import api from "../config/api";
+import { BASE_URL } from "../constants";
 
 export const uploadProfilePicture = async (file) => {
   const formData = new FormData();
-  formData.append("profile_picture", file);
+  formData.append('profile_picture', file);
 
-  try {
-    const response = await api.post(
-      `auth/profile/upload-profile-picture`,
-      formData,
-      {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }
-    );
-
-    const data = response.data;
-    if (response.status !== 200) {
-      toast.error(data.message || "Failed to upload profile picture");
-    }
-
-    // Update the profile picture URL in localStorage
-    const fullImageUrl = `http://127.0.0.1:8000${data.profile_picture}`;
-    localStorage.setItem("profilePicture", fullImageUrl);
-
-    return data;
-  } catch (error) {
-    toast.error("Error uploading profile picture:", error);
-    return null;
-  }
+  const { data } = await api.post(`${BASE_URL}auth/profile/upload-profile-picture`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  return data;
 };
 
 export const deleteProfilePicture = async () => {
-  try {
-    const response = await api.delete(
-      `auth/profile/delete-profile-picture`,
-    );
-    localStorage.removeItem("profilePicture");
-
-    return response.data;
-  } catch (error) {
-    toast.error("Error deleting profile picture:", error);
-    return null;
-  }
+  const { data } = await api.delete(`${BASE_URL}auth/profile/delete-profile-picture`);
+  return data;
 };
