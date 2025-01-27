493c038dcac5db406b861bc1b8a12f38b263a7fb
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-27 16:54:13 +0200

    use ErrorHandler and PageNotFound in StudentRoutes

--- src/routes/StudentRoutes.jsx
+++ src/routes/StudentRoutes.jsx
@@ -4,14 +4,19 @@ import { Route } from 'react-router-dom';
 import SecureRoute from './SecureRoute';
 import StudentLayout from '../layouts/StudentLayout';
 import Dashboard from '../pages/admin/Dashboard';
+import ErrorHandler from '../components/errors/ErrorHandler';
+import PageNotFound from '../pages/PageNotFound';
 
 const StudentRoutes = () => {
   return (
     <Routes>
-      <Route element={<SecureRoute allowedRoles={["user"]} />}>
+      <Route element={<SecureRoute allowedRoles={["user"]} />} errorElement={<ErrorHandler />}>
         <Route element={<StudentLayout />}>
           <Route path="dashboard" element={<Dashboard />} />
           {/* Add more student-specific routes */}
+
+          {/* Page not found */}
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Route>
     </Routes>
