0456ecc1ffed675664b29661d765ba8997b27808
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-27 16:53:59 +0200

    use ErrorHandler and PageNotFound in ProfessorRoutes

--- src/routes/ProfessorRoutes.jsx
+++ src/routes/ProfessorRoutes.jsx
@@ -4,14 +4,22 @@ import { Route } from "react-router-dom";
 import ProfessorLayout from "../layouts/ProfessorLayout";
 import Dashboard from "./../pages/professor/Dashboard";
 import SecureRoute from "./SecureRoute";
+import ErrorHandler from "../components/errors/ErrorHandler";
+import PageNotFound from "../pages/PageNotFound";
 
 const ProfessorRoutes = () => {
   return (
     <Routes>
-      <Route element={<SecureRoute allowedRoles={["professor"]} />}>
+      <Route
+        element={<SecureRoute allowedRoles={["professor"]} />}
+        errorElement={<ErrorHandler />}
+      >
         <Route element={<ProfessorLayout />}>
           <Route path="dashboard" element={<Dashboard />} />
           {/* Add more professor-specific routes */}
+
+          {/* Page not found */}
+          <Route path="*" element={<PageNotFound />} />
         </Route>
       </Route>
     </Routes>
