3c422c3b3713a2a4f62fb3815d99f9779a62b72d
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-28 20:52:07 +0200

    make fetchUsers function to get all users

--- src/pages/admin/AllUsers.jsx
+++ src/pages/admin/AllUsers.jsx
@@ -1,5 +1,15 @@
-import React, { useState } from 'react'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../components/ui/Table';
+import React, { useEffect, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../../components/ui/Table";
+import { getAllUsers } from "../../services/adminService";
+import { stat } from "./../../../node_modules/@types/node/fs.d";
+import { use } from "react";
 
 const AllUsers = () => {
   const [users, setUsers] = useState([]);
@@ -7,8 +17,42 @@ const AllUsers = () => {
     current_page: 1,
     total_pages: 1,
     total_items: 0,
-  })
+  });
   const [isLoading, setIsLoading] = useState(false);
+
+  const fetchUsers = async (page) => {
+    setIsLoading(true);
+    try {
+      const response = await getAllUsers(page);
+
+      if (response.status === 200) {
+        setUsers(response.data);
+        setPagination(response.pagination);
+      } else {
+        toast.error(response.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers(1);
+  }, []);
+
+  const handlePageChange = (page) => {
+    fetchUsers(page);
+  };
+
+  if (isLoading && users.length === 0) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        Loading...
+      </div>
+    );
+  }
   return (
     <div className="p-4">
       <Table>
@@ -32,7 +76,7 @@ const AllUsers = () => {
         </TableBody>
       </Table>
     </div>
-  )
-}
+  );
+};
 
-export default AllUsers
+export default AllUsers;
