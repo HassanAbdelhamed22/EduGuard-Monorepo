5b9607090ffb0d6e7aa95668c5cc4c09afc50cb2
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-28 20:41:48 +0200

    create users, pagination, and isLoading states

--- src/pages/admin/AllUsers.jsx
+++ src/pages/admin/AllUsers.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../../components/ui/Table';
 
 const AllUsers = () => {
-  const data = [
-    { name: "Apple MacBook Pro 17", color: "Silver", category: "Laptop", price: "$2999" },
-    { name: "Microsoft Surface Pro", color: "White", category: "Laptop PC", price: "$1999" },
-    { name: "Magic Mouse 2", color: "Black", category: "Accessories", price: "$99" },
-  ];
+  const [users, setUsers] = useState([]);
+  const [pagination, setPagination] = useState({
+    current_page: 1,
+    total_pages: 1,
+    total_items: 0,
+  })
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div className="p-4">
       <Table>
