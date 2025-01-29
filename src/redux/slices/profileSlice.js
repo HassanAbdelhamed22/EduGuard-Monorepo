47d501b990c12f7b04c5ec6a939fde0e5ee9f5a0
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-29 08:06:03 +0200

    Async thunk to fetch profile

--- src/redux/slices/profileSlice.js
+++ src/redux/slices/profileSlice.js
@@ -1,7 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getProfile } from "../../services/authService";
+import toast from "react-hot-toast";
+
+// Async thunk to fetch profile
+export const fetchProfile = createAsyncThunk(
+  "profile/fetchProfile",
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await getProfile();
+      if (data?.profile_picture) {
+        data.profile_picture = `http://127.0.0.1:8000/storage/${
+          data.profile_picture
+        }?${new Date().getTime()}`;
+      }
+      return data;
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+      toast.error("Error fetching profile");
+      return rejectWithValue(error.message);
+    }
+  }
+);
 
 const initialState = {
-  profile: null,
+  profile: {
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    profile_picture: null, // Ensure this is set correctly
+  },
   isLoading: false,
   error: null,
 };
@@ -13,6 +41,21 @@ const profileSlice = createSlice({
     setProfile: (state, action) => {
       state.profile = action.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProfile.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchProfile.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.profile = action.payload;
+      })
+      .addCase(fetchProfile.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
   },
 });
 
