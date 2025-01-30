ec8c6740cef3b9b4bb5399fac7c4b341c47f07e7
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-30 19:19:21 +0200

    update imports in pagination logic component

--- src/components/PaginationLogic.jsx
+++ src/components/PaginationLogic.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Pagination,
   PaginationContent,
@@ -6,58 +6,58 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from "../../components/ui/Pagination";
+} from "./ui/Pagination";
 
-const PaginationLogic = ({pagination, handlePageChange}) => {
+const PaginationLogic = ({ pagination, handlePageChange }) => {
   return (
     <Pagination className="mt-8">
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious
-              onClick={() =>
-                pagination.current_page > 1 &&
-                handlePageChange(pagination.current_page - 1)
-              }
-              className={
-                pagination.current_page <= 1
-                  ? "pointer-events-none opacity-50"
-                  : "cursor-pointer"
-              }
-            />
-          </PaginationItem>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            onClick={() =>
+              pagination.current_page > 1 &&
+              handlePageChange(pagination.current_page - 1)
+            }
+            className={
+              pagination.current_page <= 1
+                ? "pointer-events-none opacity-50"
+                : "cursor-pointer"
+            }
+          />
+        </PaginationItem>
 
-          {Array.from({ length: pagination.total_pages }, (_, i) => i + 1).map(
-            (page) => (
-              <PaginationItem key={page}>
-                <PaginationLink
-                  onClick={() => handlePageChange(page)}
-                  isActive={page === pagination.current_page}
-                  className={`${
-                    page === pagination.current_page ? "" : "cursor-pointer"
-                  }`}
-                >
-                  {page}
-                </PaginationLink>
-              </PaginationItem>
-            )
-          )}
+        {Array.from({ length: pagination.total_pages }, (_, i) => i + 1).map(
+          (page) => (
+            <PaginationItem key={page}>
+              <PaginationLink
+                onClick={() => handlePageChange(page)}
+                isActive={page === pagination.current_page}
+                className={`${
+                  page === pagination.current_page ? "" : "cursor-pointer"
+                }`}
+              >
+                {page}
+              </PaginationLink>
+            </PaginationItem>
+          )
+        )}
 
-          <PaginationItem>
-            <PaginationNext
-              onClick={() =>
-                pagination.current_page < pagination.total_pages &&
-                handlePageChange(pagination.current_page + 1)
-              }
-              className={
-                pagination.current_page >= pagination.total_pages
-                  ? "pointer-events-none opacity-50"
-                  : "cursor-pointer"
-              }
-            />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
-  )
-}
+        <PaginationItem>
+          <PaginationNext
+            onClick={() =>
+              pagination.current_page < pagination.total_pages &&
+              handlePageChange(pagination.current_page + 1)
+            }
+            className={
+              pagination.current_page >= pagination.total_pages
+                ? "pointer-events-none opacity-50"
+                : "cursor-pointer"
+            }
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+};
 
-export default PaginationLogic
+export default PaginationLogic;
