d5824d1684f2966c4f68060a71c159f5c2c0ab99
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-31 08:31:28 +0200

    extracted components and hooks in the main AllStudents component

--- src/pages/admin/AllStudents.jsx
+++ src/pages/admin/AllStudents.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import useStudents from "./../../hooks/allStudents/useStudents";
+import AllStudentsTable from "../../components/Tables/AllStudentsTable";
+import PaginationLogic from "../../components/PaginationLogic";
 
 const AllStudents = () => {
-  return (
-    <div>AllStudents</div>
-  )
-}
+  const { students, pagination, isLoading, fetchStudents } = useStudents();
 
-export default AllStudents
+  const handlePageChange = (page) => {
+    if (
+      page !== pagination.current_page &&
+      page > 0 &&
+      page <= pagination.total_pages
+    ) {
+      fetchUsers(page);
+    }
+  };
+
+  const handleBlockToggle = (studentId, newStatus) => {
+    // Call API to update block status
+    console.log(
+      `Student ${studentId} is now ${newStatus ? "Blocked" : "Unblocked"}`
+    );
+  };
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold text-darkGray">All Students</h2>
+      </div>
+
+      <AllStudentsTable students={students} onBlockToggle={handleBlockToggle} />
+
+      <PaginationLogic
+        pagination={pagination}
+        handlePageChange={handlePageChange}
+      />
+    </div>
+  );
+};
+
+export default AllStudents;
