425ff4a1f9d9469592ea1e2798340c2fc0b802ae
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-31 19:24:36 +0200

    create the ui and call CreateUserAccountForm

--- src/pages/admin/CreateUserAccount.jsx
+++ src/pages/admin/CreateUserAccount.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { createUserAccount } from "../../services/adminService";
+import { createUserAccountValidationSchema } from "../../utils/validation";
+import CreateUserAccountForm from "../../components/forms/createUserAccountForm";
 
 const CreateUserAccount = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -15,28 +17,48 @@ const CreateUserAccount = () => {
     password_confirmation: "",
     phone: "",
     address: "",
-    role: "student",
+    role: "user",
   };
 
   const handleSubmit = async (values) => {
     setIsLoading(true);
+    console.log("Submitting values:", values);
     try {
       const { data, status } = await createUserAccount(values);
 
       if (status === 201) {
         toast.success(data.message);
+      
       } else {
         toast.error("Unexpected server response. Please try again.");
       }
     } catch (error) {
       const errorMessage =
-        error.response?.data?.message ||
-        "Something went wrong. Please try again.";
+        error.response?.data?.message ;
       toast.error(errorMessage);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  return <div></div>;
+  return (
+    <div className="max-w-xl mx-auto p-6 rounded-lg shadow-md bg-white mt-20">
+      <h2 className="text-2xl font-semibold mb-6 pb-4 text-center border-b text-primary">
+        Create User Account
+      </h2>
+
+      <CreateUserAccountForm
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={createUserAccountValidationSchema}
+        isLoading={isLoading}
+        showPassword={showPassword}
+        setShowPassword={setShowPassword}
+        showPasswordConfirmation={showPasswordConfirmation}
+        setShowPasswordConfirmation={setShowPasswordConfirmation}
+      />
+    </div>
+  );
 };
 
 export default CreateUserAccount;
