9950fcfe1c4fde21b2802cbb614187a3578774a1
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-01-31 10:18:42 +0200

    make states and fetchProfessors function

--- src/hooks/allProfessors/useProfessors.js
+++ src/hooks/allProfessors/useProfessors.js
@@ -1,3 +1,33 @@
-const useProfessors = () => {}
+import { useEffect, useState } from "react";
+import { getAllProfessors } from "../../services/adminService";
 
-export default useProfessors
+const useProfessors = () => {
+  const [professors, setProfessors] = useState([]);
+  const [pagination, setPagination] = useState({
+    current_page: 1,
+    total_pages: 1,
+    total_items: 0,
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchProfessors = async (page) => {
+    setIsLoading(true);
+    try {
+      const { professors, pagination } = await getAllProfessors(page);
+      setProfessors(professors);
+      setPagination({ ...pagination, current_page: page });
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProfessors(1);
+  }, []);
+
+  return { professors, pagination, isLoading, fetchProfessors };
+};
+
+export default useProfessors;
