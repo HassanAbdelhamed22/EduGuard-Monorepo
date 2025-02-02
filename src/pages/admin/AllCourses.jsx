09f0adda3fd21b212046abf252ab2318158e1fc4
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-02 07:04:38 +0200

    extracted components and hooks in the main AllCourses component

--- src/pages/admin/AllCourses.jsx
+++ src/pages/admin/AllCourses.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import useCourses from "../../hooks/allCourses/useCourses";
+import useModal from "./../../hooks/allCourses/useModal";
+import Loading from "../../components/ui/Loading";
+import AllCoursesTable from "../../components/Tables/AllCoursesTable";
+import PaginationLogic from "../../components/PaginationLogic";
 
 const AllCourses = () => {
-  return (
-    <div>AllCourses</div>
-  )
-}
+  const { courses, pagination, isLoading, fetchCourses } = useCourses();
+  const { modal, openModal, closeModal } = useModal();
 
-export default AllCourses
+  const handlePageChange = (page) => {
+    if (
+      page !== pagination.current_page &&
+      page > 0 &&
+      page <= pagination.total_pages
+    ) {
+      fetchCourses(page);
+    }
+  };
+
+  if (isLoading && courses.length === 0) {
+    return <Loading />;
+  }
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold text-darkGray">All Courses</h2>
+      </div>
+
+      <AllCoursesTable courses={courses} />
+
+      <PaginationLogic
+        pagination={pagination}
+        handlePageChange={handlePageChange}
+      />
+    </div>
+  );
+};
+
+export default AllCourses;
