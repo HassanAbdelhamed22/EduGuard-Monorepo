0777d4f0bc92de244b49be0ecccc04420847f4a4
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-03 05:22:21 +0200

    add routes for professor to ProfessorRoutes

--- src/routes/ProfessorRoutes.jsx
+++ src/routes/ProfessorRoutes.jsx
@@ -6,6 +6,8 @@ import Dashboard from "./../pages/professor/Dashboard";
 import SecureRoute from "./SecureRoute";
 import ErrorHandler from "../components/errors/ErrorHandler";
 import PageNotFound from "../pages/PageNotFound";
+import Profile from "../pages/auth/Profile";
+import UpdatePassword from "../pages/auth/UpdatePassword";
 
 const ProfessorRoutes = () => {
   return (
@@ -16,9 +18,31 @@ const ProfessorRoutes = () => {
       >
         <Route element={<ProfessorLayout />}>
           <Route path="dashboard" element={<Dashboard />} />
-          {/* Add more professor-specific routes */}
+          <Route path="courses" element={<div>My Courses</div>} />
+          <Route
+            path="courses/upload-material"
+            element={<div>Upload Material</div>}
+          />
+          <Route
+            path="courses/manage-material"
+            element={<div>Manage Course Material</div>}
+          />
 
-          {/* Page not found */}
+          <Route path="quizzes" element={<div>My Quizzes</div>} />
+          <Route path="quizzes/create" element={<div>Create Quiz</div>} />
+          <Route
+            path="quizzes/add-questions"
+            element={<div>Add Questions</div>}
+          />
+          <Route
+            path="quizzes/manage-questions"
+            element={<div>Manage Questions</div>}
+          />
+          <Route path="quizzes/view-list" element={<div>View Quiz List</div>} />
+
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/update-password" element={<UpdatePassword />} />
+
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Route>
