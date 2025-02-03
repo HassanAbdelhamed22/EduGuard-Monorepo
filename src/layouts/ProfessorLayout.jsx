8979ff5b4987e00ec3db3009b79e4df036a35b94
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-03 04:58:08 +0200

    update professor layout

--- src/layouts/ProfessorLayout.jsx
+++ src/layouts/ProfessorLayout.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import Sidebar from './../components/Sidebar';
 
 const ProfessorLayout = () => {
   return (
-    <div className="professor-layout">
-      <Header />
-      <main>
-        <Outlet />
+    <div className="min-h-screen flex flex-col">
+      <main className="flex flex-1">
+        {/* Sidebar */}
+        <aside className="w-64 hidden lg:block">
+          <Sidebar />
+        </aside>
+
+        {/* Main Content */}
+        <div className="flex-1 flex flex-col h-screen">
+          {/* Header */}
+          <div className="sticky top-0 z-50 w-full">
+            <Header />
+          </div>
+
+          {/* Scrollable Outlet Content */}
+          <div className="flex-1 px-6 py-4 overflow-y-auto">
+            <Outlet />
+          </div>
+        </div>
       </main>
-      <footer>Professor Footer</footer>
     </div>
   );
 };
