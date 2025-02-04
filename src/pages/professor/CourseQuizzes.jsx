bed9a9d5b6b295cecef9d877901cb8d2379e3b40
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-04 19:17:25 +0200

    fetch quizzes in CourseQuizzes page

--- src/pages/professor/CourseQuizzes.jsx
+++ src/pages/professor/CourseQuizzes.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import { viewCourseQuizzes } from '../../services/professorService';
+import Loading from '../../components/ui/Loading';
 
 const CourseQuizzes = () => {
+  const { courseId } = useParams();
+  const [quizzes, setQuizzes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchQuizzes = async () => {
+    try {
+      const { data } = await viewCourseQuizzes(courseId);
+      setQuizzes(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuizzes();
+  }, [courseId]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <div>CourseQuizzes</div>
   )
