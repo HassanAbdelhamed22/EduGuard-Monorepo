8cde2063259f584a9113b0934b06f72898152ed6
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-04 23:35:11 +0200

    make fetchMaterials function

--- src/pages/professor/CourseMaterials.jsx
+++ src/pages/professor/CourseMaterials.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
+import { viewCourseMaterials } from "../../services/professorService";
 
 const CourseMaterials = () => {
   const { courseId } = useParams();
+  const [materials, setMaterials] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const location = useLocation();
   const { courseName, courseCode } = location.state || {
     courseName: "Unknown Course",
     courseCode: "N/A",
   };
+
+  const fetchMaterials = async () => {
+    try {
+      const data = await viewCourseMaterials(courseId);
+      setMaterials(data);
+    } catch (error) {
+      console.error("Error fetching materials:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMaterials();
+  }, [courseId]);
   return (
     <div className="container p-4 mx-auto">
       <div className="mb-6">
