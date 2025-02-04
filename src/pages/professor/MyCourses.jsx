a4ce1012de01b46b665232a462243f00cf2fc846
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-04 07:09:52 +0200

    add created at and last updated to UI

--- src/pages/professor/MyCourses.jsx
+++ src/pages/professor/MyCourses.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Button from "../../components/ui/Button";
 import { viewRegisteredCourses } from "../../services/professorService";
-import Loading from './../../components/ui/Loading';
+import Loading from "./../../components/ui/Loading";
 const CourseList = () => {
   const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -23,14 +23,12 @@ const CourseList = () => {
   }, []);
 
   if (loading) {
-    return (
-      <Loading />
-    );
+    return <Loading />;
   }
 
   return (
     <div className="container p-4 mx-auto">
-      <div className="flex gap-4">
+      <div className="flex items-center gap-4 mb-4">
         {courses.map((course) => (
           <div
             key={course.CourseID}
@@ -39,14 +37,22 @@ const CourseList = () => {
             <div className="flex items-center justify-between mb-2">
               <h3 className="text-lg font-bold">{course.CourseCode}</h3>
               <span className="px-2 py-1 text-xs text-primary bg-indigo-50 rounded-full">
-                {course.course_registrations?.length} students
+                {course.course_registrations?.length > 0
+                  ? `${course.course_registrations.length} students`
+                  : "No students yet"}
               </span>
             </div>
             <hr className="mb-2 border-gray-300" />
-            <p className="mb-3 font-medium text-gray-600 text-md">
+            <p className="mb-3 font-medium text-gray-700 text-md">
               {course.CourseName}
             </p>
-            <div className="flex gap-2 mb-3">
+            <p className="text-sm text-gray-500">
+              Created: {new Date(course.created_at).toLocaleDateString()}
+            </p>
+            <p className="text-sm text-gray-500 mb-3">
+              Last Updated: {new Date(course.updated_at).toLocaleDateString()}
+            </p>
+            <div className="flex gap-2">
               <Button variant={"default"}>View Quizzes</Button>
 
               <Button variant={"outline"}> View Materials</Button>
