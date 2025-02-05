372ddd08ab03fabb32570b5cb019c6b90a0e7f80
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-05 14:42:05 +0200

    update grid in Section component

--- src/components/ui/Section.jsx
+++ src/components/ui/Section.jsx
@@ -11,7 +11,7 @@ const Section = ({ title, materials, icon: Icon, iconColor }) => {
       {materials.length === 0 ? (
         <p className="text-gray-600">No {title.toLowerCase()} available.</p>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
           {materials.map((material) => (
             <MaterialCard key={material.MaterialID} material={material} />
           ))}
