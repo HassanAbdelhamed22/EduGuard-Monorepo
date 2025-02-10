ce6cc4c34bc52aaf8b1462eec88d5ee819a648fc
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-10 23:50:50 +0200

    create checkbox reusable component

---
+++ src/components/ui/Checkbox.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Checkbox } from "@headlessui/react";
+
+const CustomCheckbox = ({ checked, onChange }) => {
+  return (
+    <Checkbox
+      checked={checked}
+      onChange={onChange}
+      className="group block size-5 rounded border bg-gray-100 data-[checked]:bg-primary"
+    >
+      {/* Checkmark icon */}
+      <svg
+        className="stroke-white opacity-0 group-data-[checked]:opacity-100"
+        viewBox="0 0 14 14"
+        fill="none"
+      >
+        <path
+          d="M3 8L6 11L11 3.5"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </Checkbox>
+  );
+};
+
+export default CustomCheckbox;
