70e694c7281af9b34568533fdca4f2df4247219d
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-10 17:17:58 +0200

    add routes to student routes

--- src/routes/StudentRoutes.jsx
+++ src/routes/StudentRoutes.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
-import { Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import SecureRoute from './SecureRoute';
-import StudentLayout from '../layouts/StudentLayout';
-import ErrorHandler from '../components/errors/ErrorHandler';
-import PageNotFound from '../pages/PageNotFound';
-import Dashboard from '../pages/student/Dashboard';
+import React from "react";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import SecureRoute from "./SecureRoute";
+import StudentLayout from "../layouts/StudentLayout";
+import ErrorHandler from "../components/errors/ErrorHandler";
+import PageNotFound from "../pages/PageNotFound";
+import Dashboard from "../pages/student/Dashboard";
+import Profile from "../pages/auth/Profile";
+import UpdatePassword from "../pages/auth/UpdatePassword";
 
 const StudentRoutes = () => {
   return (
     <Routes>
-      <Route element={<SecureRoute allowedRoles={["user"]} />} errorElement={<ErrorHandler />}>
+      <Route
+        element={<SecureRoute allowedRoles={["user"]} />}
+        errorElement={<ErrorHandler />}
+      >
         <Route element={<StudentLayout />}>
           <Route path="dashboard" element={<Dashboard />} />
-          {/* Add more student-specific routes */}
+          <Route path="courses" element={<div>Course Registration</div>} />
+          <Route path="my-courses" element={<div>My Courses</div>} />
+          <Route path="quizzes" element={<div>Quizzes</div>} />
+          <Route path="quiz-results" element={<div>Quizzes Results</div>} />
+
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/update-password" element={<UpdatePassword />} />
 
           {/* Page not found */}
           <Route path="*" element={<PageNotFound />} />
         </Route>
       </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default StudentRoutes
+export default StudentRoutes;
