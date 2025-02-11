cd0900d3ad796c538d112be21d5fdfb523087cee
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-11 07:10:31 +0200

    update getRegisteredCourses function

--- src/services/studentService.js
+++ src/services/studentService.js
@@ -2,23 +2,17 @@ import toast from "react-hot-toast";
 import api from "../config/api";
 import { BASE_URL } from "../constants";
 
- export const viewRegisteredCourses = async () =>{
-    try {
-        const {data} = await api.get(`${BASE_URL}student/courses`)
-        return data
-    } catch(error){
-        console.error(error);
-        toast.error(error?.response?.data?.message)
-    }
+export const getRegisteredCourses = async () => {
+  const response = await api.get(`${BASE_URL}student/courses`);
+  return response.data;
+};
 
- }
- export const getStudntQuiz =  async () =>{
-    try{
-        const{data} = await api.get(`${BASE_URL}quiz/student-quizzes`)
-        return data
-    } catch(error){
-            console.error(error);
-            toast.error(error?.response?.data?.message)
-    }
-
- }
+export const getStudentQuiz = async () => {
+  try {
+    const { data } = await api.get(`${BASE_URL}quiz/student-quizzes`);
+    return data;
+  } catch (error) {
+    console.error(error);
+    toast.error(error?.response?.data?.message);
+  }
+};
