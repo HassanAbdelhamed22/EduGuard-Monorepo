e6cb7502f18b126aa55387ec159dd11dcf40e016
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-12 07:37:42 +0200

    Fetch quiz questions and start the quiz

--- src/pages/student/QuizInterface.jsx
+++ src/pages/student/QuizInterface.jsx
@@ -1,6 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+import { getQuizQuestions, startQuiz } from '../../services/studentService';
 
 const QuizInterface = () => {
+  const { quizId } = useParams();
+  const navigate = useNavigate();
+  const [questions, setQuestions] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const [timeLeft, setTimeLeft] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [quizStarted, setQuizStarted] = useState(false);
+
+  //** Fetch quiz questions and start the quiz
+  useEffect(() => {
+    const initializeQuiz = async () => {
+      setIsLoading(true);
+      try{
+        const startResponse = await startQuiz(quizId);
+
+        if (startResponse.status === 200) {
+          setQuizStarted(true);
+          const questionResponse = await getQuizQuestions(quizId, 1);
+          setQuestions(questionResponse.questions);
+          setTotalPages(questionResponse.pagination.totalPages);
+          setTimeLeft(startResponse.quiz.Duration * 60)
+      } else {
+        toast.error(startResponse.message);
+        navigate(-1)
+      }
+      } catch (error) {
+        console.error(error);
+        toast.error(error.message);
+        navigate(-1)
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    initializeQuiz();
+  }, [quizId, navigate]);
+
   return (
     <div>QuizInterface</div>
   )
