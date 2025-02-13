348aa1375edcbc0956c2d6077f98d68e1f6d19f9
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-13 20:15:12 +0200

    create fetchQuizDetails and handlePageChange functions

--- src/pages/student/QuizzesResults.jsx
+++ src/pages/student/QuizzesResults.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import { getSubmittedQuizzes } from "../../services/studentService";
 
 const QuizzesResults = () => {
-  return (
-    <div>QuizzesResults</div>
-  )
-}
+  const [quizDetails, setQuizDetails] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [pagination, setPagination] = useState({
+    current_page: 1,
+    total_pages: 1,
+    total_items: 0,
+  });
 
-export default QuizzesResults
+  const handlePageChange = (page) => {
+    if (
+      page !== pagination.current_page &&
+      page > 0 &&
+      page <= pagination.total_pages
+    ) {
+      fetchQuizDetails(page);
+    }
+  };
+
+  const fetchQuizDetails = async () => {
+    try {
+      const { data, pagination } = await getSubmittedQuizzes();
+      setQuizDetails(data);
+      setPagination({ ...pagination, current_page: page });
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return <div>QuizzesResults</div>;
+};
+
+export default QuizzesResults;
