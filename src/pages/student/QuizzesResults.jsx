7d0f745d00f12c1eec748b05af0484fbc9d0ee38
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-13 20:35:43 +0200

    create ui for quizzes results page

--- src/pages/student/QuizzesResults.jsx
+++ src/pages/student/QuizzesResults.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getSubmittedQuizzes } from "../../services/studentService";
+import { FileQuestion } from "lucide-react";
+import Loading from "../../components/ui/Loading";
+import Button from "../../components/ui/Button";
 
 const QuizzesResults = () => {
   const [quizDetails, setQuizDetails] = useState([]);
@@ -20,18 +23,92 @@ const QuizzesResults = () => {
     }
   };
 
-  const fetchQuizDetails = async () => {
+  const fetchQuizDetails = async (page) => {
     try {
-      const { data, pagination } = await getSubmittedQuizzes();
-      setQuizDetails(data);
-      setPagination({ ...pagination, current_page: page });
+      const response = await getSubmittedQuizzes(page);
+      setQuizDetails(response.data);
+      setPagination({ ...response.pagination, current_page: page });
     } catch (error) {
       toast.error(error.message);
     } finally {
       setIsLoading(false);
     }
   };
-  return <div>QuizzesResults</div>;
+
+  useEffect(() => {
+    fetchQuizDetails(pagination.current_page);
+  }, []);
+
+  if (isLoading) {
+    <Loading />;
+  }
+  return (
+    <div className="container p-4 mx-auto">
+      <h1 className="text-2xl font-semibold mb-4">Quizzes Results</h1>
+      {quizDetails.length === 0 ? (
+        <p className="text-gray-600">No results available.</p>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {quizDetails.map((quiz) => (
+            <div
+              key={quiz.quiz_details.id}
+              className="p-6 border rounded-lg shadow-sm hover:shadow-lg transition-shadow bg-white cursor-pointer flex flex-col h-full justify-between"
+            >
+              <div className="flex items-center gap-4">
+                <div className="p-3 bg-indigo-50 rounded-full">
+                  <FileQuestion className="w-6 h-6 text-primary" />
+                </div>
+                <div>
+                  <h2 className="text-xl font-semibold text-gray-800">
+                    {quiz.quiz_details.title}
+                  </h2>
+                  <p className="text-sm text-gray-600">
+                    {quiz.course_details.name} ({quiz.course_details.code})
+                  </p>
+                  <div className="mt-2 text-sm text-gray-500">
+                    <p>
+                      Date:{" "}
+                      {new Date(
+                        quiz.quiz_details.quiz_date
+                      ).toLocaleDateString()}
+                    </p>
+                    <div className="flex gap-4">
+                      <p>
+                        Start:{" "}
+                        {new Date(
+                          quiz.quiz_details.start_time
+                        ).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </p>
+                      <p>
+                        End:{" "}
+                        {new Date(
+                          quiz.quiz_details.end_time
+                        ).toLocaleTimeString([], {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        })}
+                      </p>
+                    </div>
+                    <p>Duration: {quiz.quiz_details.duration} minutes</p>
+                    <p>Total Marks: {quiz.quiz_details.total_marks} marks</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-4">
+                <Button variant="default" fullWidth>
+                  Show Result
+                </Button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default QuizzesResults;
