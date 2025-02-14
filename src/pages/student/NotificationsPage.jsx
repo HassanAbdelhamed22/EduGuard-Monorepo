5c142f237076795746913ef266850967c2ce06ec
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-14 21:46:55 +0200

    Fetch notifications and unread count

--- src/pages/student/NotificationsPage.jsx
+++ src/pages/student/NotificationsPage.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  getAllNotifications,
+  getUnreadNotifications,
+} from "../../services/notificationService";
+import toast from "react-hot-toast";
 
 const NotificationsPage = () => {
-  return (
-    <div>NotificationsPage</div>
-  )
-}
+  const [notifications, setNotifications] = useState([]);
+  const [unreadCount, setUnreadCount] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
-export default NotificationsPage
+  // Fetch notifications and unread count
+  const fetchNotifications = async () => {
+    setLoading(true);
+    try {
+      const allNotifications = await getAllNotifications();
+      const unreadNotifications = await getUnreadNotifications();
+      setNotifications(allNotifications.notifications);
+      setUnreadCount(unreadNotifications.notifications.length);
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to fetch notifications");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchNotifications();
+  })
+  return <div>NotificationsPage</div>;
+};
+
+export default NotificationsPage;
