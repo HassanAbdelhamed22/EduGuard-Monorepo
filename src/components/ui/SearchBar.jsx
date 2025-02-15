4c6198450de2851056c3fa534b637d1a1c0c61a9
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-15 16:11:37 +0200

    transition-transform

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -8,7 +8,7 @@ const SearchBar = ({ value, onChange, placeholder = "Search ..." }) => {
         placeholder={placeholder}
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className="w-full pr-10 pl-4 py-3 border-[1px] border-borderLight dark:border-borderDark shadow-md focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg"
+        className="w-full pr-10 pl-4 py-3 border-[1px] border-borderLight dark:border-borderDark shadow-md focus:border-indigo-600 focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-lg focus:scale-110 duration-300 ease-in-out transform transition-transform"
       />
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -16,7 +16,7 @@ const SearchBar = ({ value, onChange, placeholder = "Search ..." }) => {
         viewBox="0 0 24 24"
         strokeWidth={2}
         stroke="currentColor"
-        className="w-6 h-6 absolute right-3 text-mediumGray dark:text-gray-300"
+        className="w-6 h-6 absolute right-3 text-mediumGray dark:text-gray-300 focus:scale-110 duration-300"
       >
         <path
           strokeLinecap="round"
