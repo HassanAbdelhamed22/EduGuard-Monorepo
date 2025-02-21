49cb11e1cb4bb5e93e5c20662dd7431ef7cb72ab
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-02-21 19:19:03 +0200

    Update the SearchBar component to accept an onEnter prop

--- src/components/ui/SearchBar.jsx
+++ src/components/ui/SearchBar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Search } from "lucide-react";
 
-const SearchBar = ({ value, onChange, placeholder = "Search..." }) => {
+const SearchBar = ({ value, onChange, onEnter, placeholder = "Search..." }) => {
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      onEnter(event);
+    }
+  };
+
   return (
     <div className="relative w-full md:w-96">
       <div className="absolute inset-y-0 left-3 flex items-center pointer-events-none">
@@ -11,6 +17,7 @@ const SearchBar = ({ value, onChange, placeholder = "Search..." }) => {
         type="text"
         value={value}
         onChange={onChange}
+        onKeyDown={handleKeyPress}
         placeholder={placeholder}
         className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 bg-white focus:outline-none"
       />
