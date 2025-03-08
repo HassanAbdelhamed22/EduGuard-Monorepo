9ca5b09e34d1de8c734a139031cf09d767419f92
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-03-08 23:15:49 +0200

    Extract the Floating Timer

--- src/pages/student/QuizInterface.jsx
+++ src/pages/student/QuizInterface.jsx
@@ -271,15 +271,7 @@ const QuizInterface = () => {
         
 
         {/* Floating Timer and Progress Bar */}
-        <div className="fixed top-4 right-4 bg-white p-4 rounded-lg shadow-md z-50">
-          <div className="flex items-center gap-3">
-            <Clock className="w-5 h-5 text-indigo-600" />
-            <p className="text-lg font-bold text-gray-800">
-              {timeFormatted.minutes}:{timeFormatted.seconds}
-            </p>
-          </div>
-          <Progress value={progressValue} className="h-2 mt-2" />
-        </div>
+        
 
         {/* Floating Question Navigation */}
         <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-white  p-4 rounded-lg shadow-md flex gap-4 z-50">
