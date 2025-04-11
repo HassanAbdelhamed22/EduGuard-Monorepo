e6782d33f9c032b079715c3030f635a7a4492338
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2025-04-11 22:11:13 +0200

    update startQuiz to accept captured frame and change request method to POST

--- src/services/studentService.js
+++ src/services/studentService.js
@@ -88,9 +88,17 @@ export const getQuizQuestions = async (quizId, page) => {
   }
 };
 
-export const startQuiz = async (quizId) => {
+export const startQuiz = async (quizId, capturedFrame) => {
   try {
-    const response = await api.get(`${BASE_URL}quizzes/start/${quizId}`);
+    const response = await api.post(
+      `${BASE_URL}quizzes/start/${quizId}`,
+      { captured_frame: capturedFrame },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     return response.data;
   } catch (error) {
     console.error("Failed to start quiz:", error);
@@ -118,9 +126,7 @@ export const submitQuiz = async (quizId, answers) => {
 
 export const getSubmittedQuizzes = async (page) => {
   try {
-    const response = await api.get(
-      `${BASE_URL}quizzes/submitted?page=${page}`
-    );
+    const response = await api.get(`${BASE_URL}quizzes/submitted?page=${page}`);
     return response.data;
   } catch (error) {
     console.error("Failed to fetch submitted quizzes:", error);
